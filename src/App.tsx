ae3c50022a38696d29180a35b0f7e12b174fcbab
santoshkhujat555-prog <santoshkhujat555@gmail.com>
2025-09-10 08:48:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ProfileWithFriends from '@/components/ProfileWithFriends/Profile';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
